ef546fff193c45a9f7d696a526d6316b3f1334dc
Calu <lucas_britez@hotmail.com>
2024-09-12 01:32:45 -0300

    empezamos validaciones en bd

--- FormoTex-Back/src/controllers/controllers.auth.ts
+++ FormoTex-Back/src/controllers/controllers.auth.ts
@@ -3,17 +3,43 @@ import { User } from '../models/model.user';
 
 export const crearUsuario = async(req: Request, res: Response) => {
 
-  const user = new User(req.body);
+  const { email, password } = req.body;
 
-  await user.save();
+  try {
 
-  res.status(201).json(
-    { 
-      ok: true,
-      message: 'Se ha creado el usuario',
-      user
-    });
-  
+    //const user = new User(req.body);
+    //await user.save();
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json(
+        { 
+          ok: false,
+          message: 'El email ya existe',
+          error: 'El email ya existe'
+        }
+      );
+    }
+    user = new User(req.body);
+    await user.save();
+
+    res.status(201).json(
+      { 
+        ok: true,
+        message: 'Se ha creado el usuario',
+        user
+      });
+    
+  } catch (error) {
+    
+    res.status(500).json(
+      { 
+        ok: false,
+        message: 'Error al crear el usuario',
+        error
+      }
+    );
+  }
 }
 
 export const loginUsuario = (req: Request, res: Response) => {
